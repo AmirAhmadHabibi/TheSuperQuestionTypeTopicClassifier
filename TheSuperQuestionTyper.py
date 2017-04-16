e628c7a4575fadadc0d5476c72afc8f2b74ef0ab
AmirAhmad <amirahmadhabibi@gmail.com>
2017-04-16 12:58:06 +0430

    count not sures

--- TheSuperQuestionTyper.py
+++ TheSuperQuestionTyper.py
@@ -5,3 +5,7 @@ import pandas as pd
 questions16 = pd.read_csv('16-javaheri.csv',  delimiter=';')
 questions17 = pd.read_csv('17-sheikholeslami.csv', delimiter=';')
 questions18 = pd.read_csv('18-sayahi.csv', delimiter=';')
+
+print 'The number of not sure subjects for 16 is ' + str(questions16['subject_notsure'].value_counts()[1])
+print 'The number of not sure subjects for 17 is ' + str(questions17['subject_notsure'].value_counts()[1])
+print 'The number of not sure subjects for 18 is ' + str(questions18['subject_notsure'].value_counts()[1])
