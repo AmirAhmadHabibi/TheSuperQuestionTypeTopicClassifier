1e1804deb80c50a6334854fec06bfde6e50fcc38
AmirAhmad <amirahmadhabibi@gmail.com>
2017-04-22 18:17:13 +0430

    fixed the problem of new lines in the data files

--- TheSuperQuestionTyper.py
+++ TheSuperQuestionTyper.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 
 def read_files():
@@ -13,14 +13,11 @@ def read_files():
     global data18
 
     # id;title;sentence;subject1;subject2;subject3;subject_notsure;suggested_subject;type1;type2;type3;type_notsure;suggested_type
-    data16 = pd.read_csv('16-javaheri.csv', delimiter=';', index_col=['id'])
-    data17 = pd.read_csv('17-sheikholeslami.csv', delimiter=';', index_col=['id'])
-    data18 = pd.read_csv('18-sayahi.csv', delimiter=';', index_col=['id'])
+    data16 = pd.read_csv('16-javaheri_e.csv', delimiter=';', index_col=['id'])
+    data17 = pd.read_csv('17-sheikholeslami_e.csv', delimiter=';', index_col=['id'])
+    data18 = pd.read_csv('18-sayahi_e.csv', delimiter=';', index_col=['id'])
 
-    data16 = data16[0:2799]
-    data18 = data18[0:2799]
-    data17 = data17.append(data16[2567:2799])
-
+    data17 = data17.append(data16[2567:])
     data17.loc[2568:2799, ('subject1', 'subject2', 'subject3', 'suggested_subject',
                            'type1', 'type2', 'type3', 'suggested_type')] = np.nan
     data17.loc[2568:2799, ('subject_notsure', 'type_notsure')] = 0
@@ -55,64 +52,47 @@ def eval_questions():
     second_score = 4
     third_score = 3
 
-    for row_id, result_row in result.iterrows():
+    for row_id, result_row in result.iterrows(): # for each question
         d16 = data16.loc[row_id]
         d17 = data17.loc[row_id]
         d18 = data18.loc[row_id]
         scores = dict()
 
-        if d16['subject1'] is not np.nan:
-            scores[d16['subject1']] = first_score
-        if d16['subject2'] is not np.nan:
-            scores[d16['subject2']] = second_score
-        if d16['subject3'] is not np.nan:
-            scores[d16['subject3']] = third_score
+        scores = add_value(d16['subject1'], first_score, scores)
+        scores = add_value(d16['subject2'], second_score, scores)
+        scores = add_value(d16['subject3'], third_score, scores)
 
-        if d17['subject1'] is not np.nan:
-            if d17['subject1'] in scores:
-                scores[d17['subject1']] += first_score
-            else:
-                scores[d17['subject1']] = first_score
-        if d17['subject2'] is not np.nan:
-            if d17['subject2'] in scores:
-                scores[d17['subject2']] += second_score
-            else:
-                scores[d17['subject2']] = second_score
-        if d17['subject3'] is not np.nan:
-            if d17['subject3'] in scores:
-                scores[d17['subject3']] += third_score
-            else:
-                scores[d17['subject3']] = third_score
+        scores = add_value(d17['subject1'], first_score, scores)
+        scores = add_value(d17['subject2'], second_score, scores)
+        scores = add_value(d17['subject3'], third_score, scores)
 
-        if d18['subject1'] is not np.nan:
-            if d18['subject1'] in scores:
-                scores[d18['subject1']] += first_score
-            else:
-                scores[d18['subject1']] = first_score
-        if d18['subject2'] is not np.nan:
-            if d18['subject2'] in scores:
-                scores[d18['subject2']] += second_score
-            else:
-                scores[d18['subject2']] = second_score
-        if d18['subject3'] is not np.nan:
-            if d18['subject3'] in scores:
-                scores[d18['subject3']] += third_score
-            else:
-                scores[d18['subject3']] = third_score
+        scores = add_value(d18['subject1'], first_score, scores)
+        scores = add_value(d18['subject2'], second_score, scores)
+        scores = add_value(d18['subject3'], third_score, scores)
 
-        sorted_subjects = sorted(scores, key=scores.get, reverse=True)
+        sorted_scores = sorted(scores, key=scores.get, reverse=True)
         try:
-            result_row['subject1'] = [sorted_subjects[0], scores[sorted_subjects[0]]]
-            result_row['subject2'] = [sorted_subjects[1], scores[sorted_subjects[1]]]
-            result_row['subject3'] = [sorted_subjects[2], scores[sorted_subjects[2]]]
+            result_row['subject1'] = [sorted_scores[0], scores[sorted_scores[0]]]
+            result_row['subject2'] = [sorted_scores[1], scores[sorted_scores[1]]]
+            result_row['subject3'] = [sorted_scores[2], scores[sorted_scores[2]]]
         except:
             pass
         result.loc[row_id] = result_row
 
-    print result[:15]
+    print result[:]
+# TODO: row2012-2013-2026
+
+
+def add_value(name, score_value, scores):
+    if name is not np.nan:
+        if name in scores:
+            scores[name] += score_value
+        else:
+            scores[name] = score_value
+    return scores
 
 
 read_files()
 # eval_subjects()
 # eval_types()
-eval_questions()
+# eval_questions()
