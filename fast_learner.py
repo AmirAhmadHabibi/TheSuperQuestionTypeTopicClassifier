b126ed1bf5fbeb13b2dddf47f69d2e101b3d4377
amirahmad <amirahmadhabibi@gmail.com>
2017-10-11 11:36:06 +0330

    fast_learner: found the probability

--- fast_learner.py
+++ fast_learner.py
@@ -3,18 +3,19 @@ from skmultilearn.problem_transform import BinaryRelevance
 from sklearn.svm import SVC
 
 wrd = pd.read_csv('1000word_vector_Q.csv')
-subj = pd.read_csv('subject_vector_Q.csv')
-type = pd.read_csv('type_vector_Q.csv')
+sub = pd.read_csv('subject_vector_Q.csv')
+typ = pd.read_csv('type_vector_Q.csv')
 
 # initialize Binary Relevance multi-label classifier
 # with an SVM classifier
 # SVM in scikit only supports the X matrix in sparse representation
 
-classifier = BinaryRelevance(classifier=SVC(), require_dense=[False, True])
+classifier = BinaryRelevance(classifier=SVC(probability=True), require_dense=[False, True])
 
 # train
-classifier.fit(wrd, subj)
+classifier.fit(wrd[10:], sub[10:])
+
+print classifier.partition
 
 # predict
-predictions = classifier.predict(wrd)
-print predictions
+print classifier.predict_proba(wrd[:1])
