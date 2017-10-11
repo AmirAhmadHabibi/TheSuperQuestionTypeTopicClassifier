832d9ad623163d7656d8fdac94c34495decfbb4e
amirahmad <amirahmadhabibi@gmail.com>
2017-10-11 15:37:57 +0330

    fast_learner: added type

--- fast_learner.py
+++ fast_learner.py
@@ -7,13 +7,13 @@ import pickle
 
 words_vector = pd.read_csv('words_vector.csv')
 subjs = pd.read_csv('subjs-result.csv', delimiter=';')
-# types = pd.read_csv('./1_combine_tags/types-result.csv', delimiter=';')
+types = pd.read_csv('types-result.csv', delimiter=';')
 
 
 def learn_svm():
     wrd = pd.read_csv('1000word_vector_Q.csv')
     sub = pd.read_csv('subject_vector_Q.csv')
-    # typ = pd.read_csv('type_vector_Q.csv')
+    typ = pd.read_csv('type_vector_Q.csv')
 
     subject_classifier = BinaryRelevance(classifier=SVC(probability=True), require_dense=[False, True])
     subject_classifier.fit(wrd, sub)
@@ -21,6 +21,15 @@ def learn_svm():
     sub_class_file = open('sub_class_file.pkl', 'wb')
     pickle.dump(subject_classifier, sub_class_file)
     sub_class_file.close()
+
+    type_classifier = BinaryRelevance(classifier=SVC(probability=True), require_dense=[False, True])
+    type_classifier.fit(wrd, typ)
+
+    typ_class_file = open('typ_class_file.pkl', 'wb')
+    pickle.dump(type_classifier, typ_class_file)
+    typ_class_file.close()
+
+    print 'Saved the pickles!'
 
 
 def tokenise(question):
@@ -36,7 +45,7 @@ def tokenise(question):
     return question.split()
 
 
-def predict_subject(subject_classifier, question):
+def predict_subject_type(subject_classifier, type_classifier, question):
     question_vector = pd.DataFrame(dtype=object)
     for wrd in words_vector['term'].as_matrix():
         question_vector[wrd] = 0
@@ -46,26 +55,37 @@ def predict_subject(subject_classifier, question):
     for word in tokenise(question):
         if word in question_vector:
             question_vector.loc[0, word] = 1
-
     question_vector = question_vector.fillna(0)
 
-    # predict
+    # predict subject
     prediction_probabilities = np.array(subject_classifier.predict_proba(question_vector).todense())[0]
-
     prediction = pd.concat([subjs['tag'], pd.DataFrame(prediction_probabilities)], axis=1)
     prediction = prediction.rename(columns={0: 'prob'})
     prediction = prediction.sort_values('prob', ascending=False)
     prediction = prediction.reset_index(drop=True)
+    print 'subject: '
+    print prediction[prediction['prob'] >= 0.1]
 
+    # predict type
+    prediction_probabilities = np.array(type_classifier.predict_proba(question_vector).todense())[0]
+    prediction = pd.concat([types['tag'], pd.DataFrame(prediction_probabilities)], axis=1)
+    prediction = prediction.rename(columns={0: 'prob'})
+    prediction = prediction.sort_values('prob', ascending=False)
+    prediction = prediction.reset_index(drop=True)
+    print 'type: '
     print prediction[prediction['prob'] >= 0.1]
 
 
-def go():
+def do_questions():
+    # load the classifiers
     sub_class_file = open('sub_class_file.pkl', 'rb')
     subject_classifier = pickle.load(sub_class_file)
+    typ_class_file = open('typ_class_file.pkl', 'rb')
+    type_classifier = pickle.load(typ_class_file)
+    # get a question and predict its subject
     question = raw_input('Enter a question:')
-    predict_subject(subject_classifier, question)
+    predict_subject_type(subject_classifier, type_classifier, question)
 
 
 # learn_svm()
-go()
+do_questions()
