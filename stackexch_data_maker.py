d453e7d8c3857880da8b9ed433895f3d24c42176
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-01-30 16:41:05 +0330

    added stackexchange_data_maker.py : find tags + build words vectors

--- stackexch_data_maker.py
+++ stackexch_data_maker.py
@@ -5,7 +5,6 @@ from sys import stdout as so
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet
 from nltk import pos_tag, word_tokenize
-from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
 
@@ -13,6 +12,7 @@ def reformat(text):
     text = text.replace(' lt ', '<')
     text = text.replace(' gt ', '>')
     text = text.replace(' quot ', '')
+    text = text.replace('\n', ' ')
     text = text.replace(' xA ', '')
     text = text.replace(' amp ', ' and ')
     text = text.replace('--', '–')
@@ -34,11 +34,18 @@ def get_tags(txt):
 
 
 def combine_all():
-    data = pd.DataFrame(columns={'body', 'title', 'tag'})
+    data = pd.DataFrame(columns=['id', 'body', 'title', 'tag'])
     for i in range(1, 6):
         fold = pd.read_excel('./StackExchange_data/Fold_' + str(i) + '.xlsx')
         data = data.append(fold)
 
+    id_num = 0
+    for i, row in data.iterrows():
+        row['id'] = id_num
+        id_num += 1
+
+    data.set_index('id')
+    bad_rows = set()
     html_cleaner = re.compile('<.*?>')
     for i, row in data.iterrows():
         try:
@@ -49,10 +56,18 @@ def combine_all():
             row['tag'] = get_tags(row['tag'])
         except Exception as e:
             print(e)
-            print(row)
-            data.drop(data.index[i])
+            print('--', i, row)
+            bad_rows.add(row['id'])
 
-    data.to_csv('./StackExchange_data/all_data.csv', index=False)
+    data = data.drop(data.index[list(bad_rows)])
+
+    # reset the id numbers
+    id_num = 0
+    for i, row in data.iterrows():
+        row['id'] = id_num
+        id_num += 1
+
+    data.to_csv('./StackExchange_data/all_data.csv', index=False, columns=['id', 'body', 'title', 'tag'])
 
 
 def get_wordnet_pos(pos):
@@ -76,12 +91,18 @@ def find_frequent_words():
     for w in stopwords.words('english'):
         stop_words.add(w)
 
+    # with open('./StackExchange_data/words50000.csv', 'r') as infile:
+    #     for line in infile:
+    #         w, _, f = line.partition(',')
+    #         words[w] = int(f)
+
     start_time = time.time()
     total_num = data.shape[0]
     cleaner = re.compile('^\s*-*|-\s*$')
 
     for i, row in data.iterrows():
-
+        # if i <= 50000:
+        #     continue
         elapsed_time = time.time() - start_time
         retime = (total_num - i - 1) * elapsed_time / (i + 1)
         so.write('\rprocessed %' + str(round(100 * (i + 1) / total_num, 2))
@@ -106,7 +127,7 @@ def find_frequent_words():
                     words[word] += 1
                 else:
                     words[word] = 1
-        # print(' ')
+
         if i % 5000 == 0:
             with open('./StackExchange_data/words' + str(i) + '.csv', 'w') as outfile:
                 for word, freq in words.items():
@@ -116,12 +137,76 @@ def find_frequent_words():
 
     with open('./StackExchange_data/words_frequency.csv', 'w') as outfile:
         for word in sorted_words:
-            outfile.write(word + ',' + words[word] + '\n')
+            try:
+                outfile.write(str(word) + ',' + str(words[word]) + '\n')
+            except:
+                pass
 
     with open('./StackExchange_data/1000words.csv', 'w') as outfile:
         for word in sorted_words[:1000]:
-            outfile.write(word + '\n')
+            outfile.write(str(word) + '\n')
+
+
+def build_word_vectors():
+    data = pd.read_csv('./StackExchange_data/all_data.csv')
+    lemmatiser = WordNetLemmatizer()
+    words_vector = pd.read_csv('./StackExchange_data/1000words.csv', header=None, names={'term'})
+
+    # create dataframe
+    train = pd.DataFrame(dtype=object)
+    for wrd in words_vector['term'].as_matrix():
+        train[wrd] = 0
+
+    cleaner = re.compile('^\s*-*|-\s*$')
+    total_num = data.shape[0]
+    start_time = time.time()
+    # build the train data
+    for i, qrow in data.iterrows():
+        elapsed_time = time.time() - start_time
+        retime = (total_num - i - 1) * elapsed_time / (i + 1)
+        so.write('\rprocessed %' + str(round(100 * (i + 1) / total_num, 2))
+                 + '\ttime: ' + str(int(elapsed_time)) + ' | ' + str(int(retime)))
+
+        train.loc[i, words_vector['term'][0]] = 0
+
+        # set occurrence values
+        tokens_pos = pos_tag(word_tokenize(qrow['body']))
+        for word_pos in tokens_pos:
+            word = word_pos[0].lower()
+            word = re.sub(cleaner, '', word)
+            word = lemmatiser.lemmatize(word=word, pos=get_wordnet_pos(word_pos[1]))
+            if word in train:
+                train.loc[i, word] = 1
+
+    print(train.shape)
+    train = train.fillna(0)
+
+    # rename columns
+    number = 0
+    for col in train:
+        train[col] = train.apply(lambda row: int(row[col]), axis='columns')
+        train = train.rename(columns={col: 'wrd' + str(number)})
+        number += 1
+    print(train.shape)
+    train.to_csv('./StackExchange_data/data_1000word.csv', index=False)
+
+
+def find_all_tags():
+    data = pd.read_csv('./StackExchange_data/all_data.csv')
+    tags = set()
+    for i, row in data.iterrows():
+        try:
+            tags = tags | eval(row['tag'])
+        except Exception as e:
+            print(e)
+            print('--', row['id'], row['tag'])
+
+    with open('./StackExchange_data/tags.csv', 'w') as outfile:
+        for tag in tags:
+            outfile.write(tag + '\n')
 
 
 # combine_all()
-find_frequent_words()
+# find_frequent_words()
+# find_all_tags()
+build_word_vectors()
